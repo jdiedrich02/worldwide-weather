f21c7db477e0268d6dfab0e6ed88cd4dd0417bce
Darkkz <jake.diedrich@gmail.com>
2024-02-01 19:33:48 -0500

    Setup API Calls

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import { environment } from '../../environments/environment';
 export class HomeComponent {
   apiKey: string = environment.apiKey;
   currentCity: string = "Ottawa, Canada";
+  lon: any = '';
+  lat: any = '';
 
   constructor(private route: ActivatedRoute) {}
 
@@ -18,17 +20,36 @@ export class HomeComponent {
     if (Object.keys(cityObj).length != 0) {
       this.currentCity = cityObj['city'] + ", " + cityObj['country'];
     }
+
+    // Get all the weather info needed for the display
+    this.getCurrentWeather();
+    this.getHourlyWeather();
+    this.getFiveDayWeather();
   }
 
   getCurrentWeather() {
-
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.currentCity}&appid=${this.apiKey}`)
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data)
+      });
   }
 
   getHourlyWeather() {
-
+    // Every 3 hours actually lol
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.currentCity}&appid=${this.apiKey}`)
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data)
+    });
   }
 
-  getSevenDayWeather() {
-    
+  getFiveDayWeather() {
+    // Split this by days and save the data
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${this.currentCity}&appid=${this.apiKey}`)
+    .then((res) => res.json())
+    .then(data => {
+      console.log(data)
+    });
   }
 }
