15f7d761523b20ca70c15eb5bc960b6f61a5e595
Darkkz <jake.diedrich@gmail.com>
2024-03-24 20:08:06 -0400

    Switch How We Call The API

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,9 @@ export class DataService {
     return this.http.get<any>('assets/cities.json').pipe(
       map((cities: any[]) => cities.filter(c => c.name.toLowerCase().includes(city.toLowerCase())))
     );
-  } 
+  }
+
+  getWeather(city: string): Observable<any> {
+    return this.http.get(`http://api.weatherapi.com/v1/forecast.json?q=${city}&key=${environment.apiKeyWeatherAPI}&days=3`);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../environments/environment';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,13 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  protected apiKey: string;
   protected currentCity: string;
 
   protected hourlyTimes: any[] = [6, 9, 12, 15, 18];
   protected hourly: any[];
   protected weatherInfo: any;
 
-  constructor(private route: ActivatedRoute) {
-    this.apiKey = environment.apiKeyWeatherAPI;
+  constructor(private route: ActivatedRoute, private dataService: DataService) {
     this.currentCity = "Ottawa, Canada";
     this.hourly = [];
     this.weatherInfo = {};
@@ -33,12 +32,10 @@ export class HomeComponent {
   }
 
   getWeatherData() {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?q=${this.currentCity}&key=${this.apiKey}&days=3`)
-    .then((res) => res.json())
-    .then(data => {
-      console.log(data)
+    this.dataService.getWeather(this.currentCity).subscribe(data => {
       this.weatherInfo = data;
-
+      
+      // Setup Hourly
       for (let hourTimes of this.hourlyTimes) {
         this.hourly.push(this.weatherInfo.forecast['forecastday'][0]['hour'][hourTimes]);
       }
